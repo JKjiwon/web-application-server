47c9f82cf833160e508d2b32c4b082cc45b33488
JKjiwon <jwkim.oa@gmail.com>
2023-10-24 21:41:20 +0900

    feat: 5장_요구사항1 구현 - 요청 데이터 HttpRequest로 분리

---
+++ src/main/java/webserver/model/HttpRequest.java
@@ -0,0 +1,73 @@
+package webserver.model;
+
+import util.HttpRequestUtils;
+import util.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+
+    private HttpMethod method;
+    private String path;
+    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> parameters = new HashMap<>();
+
+    public HttpRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        String line = br.readLine();
+        if (line == null) {
+            return;
+        }
+
+        processRequestLine(line);
+        processHeaders(br);
+
+        if (method.equals(HttpMethod.POST)) {
+            int contentLength = Integer.parseInt(getHeader("Content-Length"));
+            parameters.putAll(HttpRequestUtils.parseQueryString(IOUtils.readData(br, contentLength)));
+        }
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getHeader(String headerKey) {
+        return headers.get(headerKey);
+    }
+
+    public String getParameter(String paramKey) {
+        return parameters.get(paramKey);
+    }
+
+    private void processHeaders(BufferedReader br) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null && !line.equals("")) {
+            HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
+            headers.put(pair.getKey(), pair.getValue());
+        }
+    }
+
+    private void processRequestLine(String requestLine) {
+        String[] requestLineToken = requestLine.split(" ");
+        method = HttpMethod.valueOf(requestLineToken[0]);
+        if (requestLineToken[1].contains("?")) {
+            int index = requestLineToken[1].indexOf("?");
+            path = requestLineToken[1].substring(0, index);
+            String queryString = requestLineToken[1].substring(index + 1);
+            parameters = HttpRequestUtils.parseQueryString(queryString);
+        } else {
+            path = requestLineToken[1];
+        }
+    }
+}
---
+++ src/test/java/webserver/model/HttpRequestTest.java
@@ -0,0 +1,36 @@
+package webserver.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HttpRequestTest {
+
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void request_GET() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertThat(request.getMethod()).isEqualTo(HttpMethod.GET);
+        assertThat(request.getPath()).isEqualTo("/user/create");
+        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
+        assertThat(request.getParameter("userId")).isEqualTo("jwkim.oa");
+    }
+
+    @Test
+    public void request_POST() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertThat(request.getMethod()).isEqualTo(HttpMethod.POST);
+        assertThat(request.getPath()).isEqualTo("/user/create");
+        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
+        assertThat(request.getParameter("userId")).isEqualTo("jwkim.oa");
+    }
+}
