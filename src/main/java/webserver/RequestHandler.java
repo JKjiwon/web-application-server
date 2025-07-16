7bfe92d3fdafc08295bc52f4f2c24c5fb8b588bf
kimjiwon <jwkim.oa@gmail.com>
2025-07-16 20:49:13 +0900

    feat: 3.4.3.1 요구사항 1 - index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,23 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class RequestHandler extends Thread {
+
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -22,32 +27,44 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             DataOutputStream output = new DataOutputStream(connection.getOutputStream())) {
+            String requestLine = input.readLine();
+            String path = extractPath(requestLine);
+            byte[] body;
+            if (path.equals("/index.html")) {
+                body = Files.readAllBytes(Path.of("./webapp" + path));
+            } else {
+                body = "Hello World".getBytes(UTF_8);
+            }
+            response200Header(output, body.length);
+            responseBody(output, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private String extractPath(String requestLine) {
+        String[] parts = requestLine.split(" ");
+        return parts[1];
+    }
+
+    private void response200Header(DataOutputStream output, int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+            output.writeBytes("HTTP/1.1 200 OK\r\n");
+            output.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            output.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            output.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void responseBody(DataOutputStream output, byte[] body) {
         try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            output.write(body);
+            output.writeBytes("\r\n");
+            output.flush();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
