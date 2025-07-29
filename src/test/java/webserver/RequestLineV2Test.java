75187c5bbb520eeaf75174743c7649d4448deb35
kimjiwon <jwkim.oa@gmail.com>
2025-07-29 12:02:39 +0900

    feat: Http Request Message에서 RequestLine 분리

---
+++ src/test/java/webserver/RequestLineV2Test.java
@@ -0,0 +1,60 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RequestLineV2Test {
+
+    @Test
+    @DisplayName("생성자로 들어오는 값이 Null이면 IOException 발생")
+    public void nullLine() {
+        assertThatThrownBy(() -> new RequestLineV2(null))
+                .isInstanceOf(IOException.class)
+                .hasMessage("EOF: RequestLine isn't received");
+    }
+
+    @Test
+    @DisplayName("생성자로 잘못된 형식의 값이 들어오면 IOException 발생")
+    public void invalidRequestLine() {
+        assertThatThrownBy(() -> new RequestLineV2("GET"))
+                .isInstanceOf(IOException.class)
+                .hasMessage("Invalid RequestLine");
+    }
+
+    @Test
+    @DisplayName("RequestLine 생성")
+    public void create() throws IOException {
+        RequestLineV2 requestLine = new RequestLineV2("GET /index.html HTTP/1.1");
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/index.html");
+    }
+
+    @Test
+    @DisplayName("QueryParam 파싱")
+    public void parseQueryParam() throws IOException {
+        RequestLineV2 requestLine = new RequestLineV2("GET /user/create?userId=jwkim.oa&password=1234 HTTP/1.1");
+        Map<String, String> queryParams = requestLine.getQueryParams();
+        assertThat(queryParams.get("userId")).isEqualTo("jwkim.oa");
+        assertThat(queryParams.get("password")).isEqualTo("1234");
+    }
+
+    @Test
+    @DisplayName("GET Method 인지 확인")
+    public void isGet() throws IOException {
+        RequestLineV2 requestLine = new RequestLineV2("GET /index.html HTTP/1.1");
+        assertThat(requestLine.isGet()).isTrue();
+    }
+
+    @Test
+    @DisplayName("POST Method 인지 확인")
+    public void isPost() throws IOException {
+        RequestLineV2 requestLine = new RequestLineV2("POST /user/create HTTP/1.1");
+        assertThat(requestLine.isPost()).isTrue();
+    }
+}
