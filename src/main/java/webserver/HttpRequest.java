44ec38d42685f04e9a9076b24b74dc1d103567bc
kimjiwon <jwkim.oa@gmail.com>
2025-07-29 16:01:42 +0900

    fix: 로그 수정

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -21,6 +21,7 @@ public class HttpRequest {
 
     public HttpRequest(BufferedReader br) throws IOException {
         requestLine = extractRequestLine(br);
+        log.debug("RequestLine - method: {}, path: {}", requestLine.getMethod(), requestLine.getPath());
         headers = extractRequestHeader(br);
         extractQueryParam();
         extractFormData(br);
@@ -64,7 +65,6 @@ public class HttpRequest {
         Map<String, String> headerMap = new HashMap<>();
         String line;
         while ((line = br.readLine()) != null && !line.isEmpty()) {
-            log.debug("RequestLine - method: {}, path: {}", requestLine.getMethod(), requestLine.getPath());
             HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
             headerMap.put(pair.getKey(), pair.getValue());
         }
